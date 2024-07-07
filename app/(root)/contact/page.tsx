0ed61e4fa5e289c43f64ff70bd434919cd69cae1
Sean Yak <compacodes@gmail.com>
2024-07-07 17:00:43 -0400

    fixed contact page

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Title from "@/components/shared/title";
+import Location from "@/components/shared/location";
+import Background from "@/components/shared/background";
+
+
+async function Contact() {
+
+  return (
+    <section className='main-container'>
+      <Background />
+      <Title />
+      <Location />
+    </section>
+  );
+}
+
+export default Contact;
