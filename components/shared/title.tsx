d68c2969f8ab6c384d22039573bf0a3f0d2c9130
Sean Yak <compacodes@gmail.com>
2025-06-21 16:44:02 -0400

    upsertStyle

--- components/shared/title.tsx
+++ components/shared/title.tsx
@@ -19,12 +19,12 @@ const Title = () => {
                 once: true,
               }}
               >
-            <div className="title w-[90vw] sm:w-[70vw] p-3 pt-18 scroll-mt-28 sm:mb-7 tracking-widest">
-            <p className="mb-10 mt-4 px-4 text-center ">
+            <div className="title w-[90vw] sm:w-[70vw] p-3 pt-18 scroll-mt-28 sm:mb-7">
+            <p className="text-center ">
                 <span className=" text-[90px] font-black tracking-widest">SELF LOVE CLUB</span><br />
                 <span className=" text-[48px] font-black  md:font-semibold tracking-widest">Bodyworks & Massage Therapy</span><br />
-                <span className="titleLoc font-bold tracking-widest">2121 Wrightsville Ave.</span><br />
-                <span className="titleLoc font-black tracking-[1.1rem] md:tracking-[2.2rem]">Wilmington</span>
+                <span className="titleLoc font-bold tracking-wide md:tracking-widest">2121 Wrightsville Ave.</span><br />
+                <span className="titleLoc font-black tracking-[.5rem] md:tracking-[2.2rem]">Wilmington</span>
 
               </p>
                 <Image src="/assets/slc_hrt01.svg" width={88} height={88} alt="Self Love Club massage and bodyworks | Wilmington, NC" className="mx-auto pt-11 dark:filter dark:invert dark:brightness-[.7] dark:saturate-[1.48] dark:hue-rotate-[180deg]"/>
