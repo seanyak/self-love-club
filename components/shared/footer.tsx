c8e4570a578bd726ed3a25c3b283b7ae18f5ab3c
Sean Yak <compacodes@gmail.com>
2025-06-21 17:03:10 -0400

    fixed style issue

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#2b281b] text-[#fdfae8] pt-4 w-[100vw] absolute inset-x-0 bottom-0 ">
       <div className="container mx-auto px-4">
-        <div className="flex w-full flex-row items-center  justify-between px-24">
+        <div className="flex w-full flex-row items-center justify-between">
           <div>
             <Link href="https://book.squareup.com/appointments/ztx8b05cyeal1q/location/L74GM8H49A0WS/services?buttonTextColor=000000&color=a9fae0&locale=en&referrer=so">
               <h3 className="footer-title">BOOK</h3>
